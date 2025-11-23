d36854ab277a3e029b5e9f8927cfbdfce0777264
Olatide Enoch <163292821+olatideenoch@users.noreply.github.com>
2025-11-23 15:09:58 +0100

    Fix SQLALCHEMY_DATABASE_URI configuration

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def load_user(user_id):
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///blog-posts.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
